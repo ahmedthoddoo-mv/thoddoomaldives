c52ed1fe2e2974a0f8e7b67b0772ea1d94350dc9
ahmedthoddoo-mv <ahmedthoddoo@gmail.com>
2026-08-16 04:26:45 +0500

    Reduce Cloudflare worker bundle size

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.100.17"],
+  serverExternalPackages: ["@supabase/supabase-js", "nodemailer"],
 };
 
 export default nextConfig;
